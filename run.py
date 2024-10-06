b1897ccd653f14d97002cae0e0972333ae252438
Chima Obutte <obuttechima@rocketmail.com>
2024-10-06 05:17:24 +0000

    Create get_scores_data function to get data string from users

--- run.py
+++ run.py
@@ -12,7 +12,22 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("employee_ratings")
 
-employee_survey_data = SHEET.worksheet("employee_survey_data")
-data = employee_survey_data.get_all_values()
 
-print(data)
+def get_scores_data():
+    """
+    Get scores figures input from the user.
+    """
+    print("Please enter your score ratings")
+    print("Data should be three numbers seperated by commas")
+    print("Scores are rated from zero (lowest score) to five (highest score)")
+    print("Example: 0,3,5\n")
+
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is {data_str}")
+
+
+
+
+
+
+get_scores_data()
